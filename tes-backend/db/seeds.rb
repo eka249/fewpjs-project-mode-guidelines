d0f06d7405578831c3b6e6ab96189389c425210c
github username <github email address>
2019-09-11 15:36:23 -0700

    svg

--- tes-backend/db/seeds.rb
+++ tes-backend/db/seeds.rb
@@ -5,24 +5,23 @@
 #
 
 #   Character.create(name: 'Luke', movie: movies.first)
+gr = "green"
+pu = "purple"
+re = "red"
+
+so = "solid"
+st = "striped"
+op = "open"
+
+di = "diamond"
+ov = "oval"
+sq = "squiggly"
+
+gso3 = Card.create(img: "tes-backend/app/images/cards/green1squiggly-open-3.jpg")
+
+
 
 psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", shading: "solid", shape: "diamond", ,  number: 1)  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
-psd1= Card.create(img:'https://cdn1.imggmi.com/uploads/2019/9/11/ee7c90bb5cc310a08aa07cd7f8948aef-full.jpg', color: "purple", 
-shape: "diamond", number: 1, shading: "squiggle")  
 
 
 # def seed_cards (color, pattern, shape, numberObj, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
