9c381acb5180e4b355c1d247b87b4d1ee5e97407
Ellyn <eka249@nyu.edu>
2019-09-11 13:55:52 -0700

    halfway through wed

--- tes-front/js/newGame.js
+++ tes-front/js/newGame.js
@@ -1,29 +1,31 @@
 document.addEventListener("DOMContentLoaded", newGame)
 
 function newGame(){
+    fetchData()
     initialRandomCards(cards)
     
 }
 
-
+function fetchData(){
+    fetch("http://localhost:3000/cards")
+    .then(response => response.json())
+    .then(cards => console.log(cards))
+}
 function initialRandomCards(cards){
     //puts default 12 cards in the grid
     //how to make sure they stay in grid format
     let cardTable= document.getElementById("grid-container")
-    idCounter=1
     x=12
     let selected = []
     const times = x => pick => {
-        let element = document.createElement("canvas")
-        index= Math.floor(Math.random() * (Card.all.length))
         //assign all coordinates in findCard function
-        findCard(index, cards)
-        element.
-        findCard(index, cards)
-        element.setAttribute(card[idCounter])
-        idCounter++
+        index= Math.floor(Math.random() * (Card.all.length))
+        let element = document.createElement("img")
+        img.src = "https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/09/dog-landing-hero-sm.jpg?bust=1536935086&width=760"
+        // element.setAttribute([idCounter])
         element.onclick= e =>{
             threeClicks(e, cards[index], selected)
+            selected.push(Card[index])
         }
         cardTable.appendChild(element)
         //how to constrain this to where a card can't be picked twice??  
@@ -41,7 +43,7 @@ function initialRandomCards(cards){
 
     document.addEventListener('click', e => {
         threeClicks(e)
-    }
+    })
 }
 
 function sideBar(){
@@ -61,38 +63,47 @@ function determineValid(arr){
     //output validYN
     submitAttempt(validYN)
 }
-function subitAttempt(validYN){
-    if (validYN === 1){
-        score++}
-    else {
-        score = score -1 
-    }
+// function subitAttempt(validYN){
+//     if (validYN === 1){
+//         score++}
+//     else {
+//         score = score -1 
+//     }
 
-    //establish connection with db, not url
-    fetch("http://localhost:3000/cards",
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body:JSON.stringify(
-            tot.score: score
-        )
-    })
-    //change value on front end to score here
-
-
-
-
-    }
-}
+//     //establish connection with db, not url
+//     fetch("http://localhost:3000/cards",
+//         method: "PATCH",
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body:JSON.stringify(
+//             tot.score: score
+//         )
+//     })
+//     //change value on front end to score here
 
 
 
 
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+////when using canvas///////////
 
 function findCard(index, cards) {
-    let addCard= canvas.getContext('2d').drawImage(cards[index].sx, cards[index].sy, cards[index].sWdith, cards[index].sHeight, cards[index].dx, cards[index].dy, cards[index].dWidth, cards[index].dHeight);
+    let addCard= canvas.getContext('2d').drawImage("https://cdn1.imggmi.com/uploads/2019/9/11/8afd7cf21d6551f71409919734470e16-full.png", cards[index].sx, cards[index].sy, cards[index].sWdith, cards[index].sHeight, cards[index].dx, cards[index].dy, cards[index].dWidth, cards[index].dHeight);
     return addCard
 }
-  
-
+///////////////////////////////////////
