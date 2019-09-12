caefa131583ef6fcc36599a171f2ce97b2877882
github username <github email address>
2019-09-12 11:01:58 -0700

    work on intialrandomcards

--- tes-front/js/newGame.js
+++ tes-front/js/newGame.js
@@ -6,30 +6,25 @@ function main(){
     fetchGames()
 }
 
-
-
-    function newGame(e){
-        e.preventDefault();
-        fetch("http://localhost:3000/games" , {
-            method: "POST",
-            headers: {
-            //    'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                totScore:0
-            })
+function newGame(e){
+    e.preventDefault();
+    fetch("http://localhost:3000/games" , {
+        method: "POST",
+        headers: {
+        //    'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            totScore:0
         })
-        .then(response => response.json()
-        )
-        // .then(testscore=> console.log(testscore))
-        //how do we connect current game with gameplay-question for after gameplay is over
-        .then(results=> submitAttempt(0, results))
-    
-    }
+    })
+    .then(response => response.json()
+    )
+    // .then(testscore=> console.log(testscore))
+    //how do we connect current game with gameplay-question for after gameplay is over
+    .then(results=> submitAttempt(0, results))
 
-
-
+}
 
 function fetchCards(){
     fetch("http://localhost:3000/cards")
@@ -37,32 +32,38 @@ function fetchCards(){
     .then(cards => initialRandomCards(cards))
 }
 
-    function initialRandomCards(cards){
-        //puts default 12 cards in the grid
-        let cardTable = document.getElementById("container")
-        let cardArr = []
-        let selected = []
-        for (i = 0; i < 12; i++) {
-            randCard= Math.floor(Math.random() * (cards.length))
-            // console.log(randCard)
-            // console.log(cards[randCard].img)
-            if (!cardArr.includes(randCard)){
-                let image = document.createElement("img")
-                image.src = cards[randCard].img
-                image.setAttribute("class", ".col-sm")
-                // image.setAttribute([idCounter])
-                image.addEventListener("click", e => {
-                    // console.log("click working")
-                    selected.push(cards[randCard])
-                    threeClicks(e, cards)
+let allCardsUsed = []
+
+function initialRandomCards(cards){
+    //puts default 12 cards in the grid
+    let cardTable = document.getElementById("container")
+    
+    let selected = []
+    
+    for (i = 0; i < 12; i++) {
+        randCard= Math.floor(Math.random() * (cards.length))
+        // console.log(randCard)
+        // console.log(cards[randCard].img)
+        if (!allCardsUsed.includes(randCard)){
+            let image = document.createElement("img")
+            image.src = cards[randCard].img
+            image.setAttribute("class", ".col-sm")
+            // image.setAttribute([idCounter])
+            image.addEventListener("click", e => {
+                // console.log("click working")
+                selected.push(cards[randCard])
+                selected.forEach(card =>{
+                    allCardsUsed.push(card)
                 })
-                cardTable.appendChild(image)
-                cardArr.push(cards[randCard])
-                // console.log(cardArr)
-                // console.log(selected)
-            }
+                threeClicks(e, cards)
+            })
+            cardTable.appendChild(image)
+            // cardArr.push(cards[randCard])
+            // console.log(cardArr)
+            // console.log(selected)
         }
     }
+}
 
 function threeClicks(e,selected){
     if (selected.length == 3) {
@@ -101,45 +102,44 @@ function determineValid(selected){
     submitAttempt(validYN)
 }
 
-    function submitAttempt(validYN, results){
+function submitAttempt(validYN, results){
 
-        console.log("results at submitattempt",results)
-        if (validYN === 1){
-            totScore++
-        }
-        else {
-            totScore = results.totScore -1 
-        }
-        console.log("totScore")
-        console.log(totScore)
-        submitNewScore(totScore)
+    console.log("results at submitattempt",results)
+    if (validYN === true){
+        totScore++
     }
-    function submitNewScore(totScore){
-        fetch("http://localhost:3000/games", {
-            method: "PATCH",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body:JSON.stringify({
-                totScore: totScore
-            })
-        })
-        let setCounter= document.getElementById("setCounter")
-        setCounter.innerHTML= totScore
-        //change value on front end to score here
-        return totScore
+    else {
+        totScore = results.totScore -1 
     }
-   
-
-    function statsScores(games){
-        sortedScores= games.sort(function(a, b){
-        return a.totScore - b.totScore;
-        });
-        // maxScores= sortedScores[0..5]
-        //too tired to find the corresponding players right now
-    }
+    console.log("totScore")
+    console.log(totScore)
+    submitNewScore(totScore)
+}
     
-
+function submitNewScore(totScore){
+    fetch("http://localhost:3000/games", {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({
+            totScore: totScore
+        })
+    })
+    let setCounter= document.getElementById("setCounter")
+    setCounter.innerHTML= totScore
+    //change value on front end to score here
+    return totScore
+}
+   
+function statsScores(games){
+    sortedScores= games.sort(function(a, b){
+    return a.totScore - b.totScore;
+    });
+    // maxScores= sortedScores[0..5]
+    //too tired to find the corresponding players right now
+}
+    
 function fetchGames(games){
     fetch("http://localhost:3000/games")
     .then(response => response.json())
