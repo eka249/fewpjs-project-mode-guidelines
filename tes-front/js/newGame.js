4c48fdff226b8e97e78aa4e349bae25e8fe6cd37
Ellyn <eka249@nyu.edu>
2019-09-12 16:15:41 -0700

    few pieces added and fixed

--- tes-front/js/newGame.js
+++ tes-front/js/newGame.js
@@ -8,20 +8,18 @@ function main(){
 
 function newGame(e){
     e.preventDefault();
+    fetchCards()
     fetch("http://localhost:3000/games" , {
         method: "POST",
         headers: {
-        //    'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             totScore:0
         })
     })
-    .then(response => response.json()
-    )
+    .then(response => response.json())
     // .then(testscore=> console.log(testscore))
-    //how do we connect current game with gameplay-question for after gameplay is over
     .then(results=> submitAttempt(0, results))
 
 }
@@ -35,49 +33,48 @@ function fetchCards(){
 let allCardsUsed = []
 
 function initialRandomCards(cards){
+    while (document.getElementById("container").hasChildNodes()){
+        document.getElementById("container").removeChild(document.getElementById("container").lastChild)
+    }
     //puts default 12 cards in the grid
     let cardTable = document.getElementById("container")
     // let allCardsUsed = []
     let selected = []
     let currentTwelve = []
-    for (i = 0; i > 12; i++) {
+    for (i = 0; i < 12; i++) {
         randCard = Math.floor(Math.random() * (cards.length))
         currentTwelve.push(cards.splice(randCard,1))
         let image = document.createElement("img")
         image.src = cards[randCard].img
         image.setAttribute("class", ".col-sm")
-        // image.setAttribute([idCounter])
         image.addEventListener("click", e => {
-            // console.log("click working")
-            console.log(e)
             selected.push(cards[randCard])
             threeClicks(e, selected)
 
         })
-        
-        
         cardTable.appendChild(image)
             // cardArr.push(cards[randCard])
             // console.log(cardArr)
             // console.log(selected)
         
         }
-    
 }
 
 
 //take selected.push out of addeventlistener
 //change threeclicks input
 
-function threeClicks(e,selected){
-    if (selected.length == 3) {
+function threeClicks(e, selected){
+    console.log("selected")
+    console.log(selected)
+    if (selected.length <= 4 ) {
         determineValid(selected)
         selected.pop()
         selected.pop()
         selected.pop()
     }
-    else {console.log(selected)
-    }
+    // else {console.log(selected)
+    // }
 }
 function determineValid(selected){
     let a = selected[0]
@@ -134,18 +131,10 @@ function submitNewScore(totScore, results){
     })
     let setCounter= document.getElementById("setCounter")
     setCounter.innerHTML= totScore
+    fetchGames()
     //change value on front end to score here
     return totScore
 }
-   
-function statsScores(games){
-    sortedScores= games.sort(function(a, b){
-    return a.totScore - b.totScore;
-    });
-    // maxScores= sortedScores[0..5]
-    //too tired to find the corresponding players right now
-}
-    
 function fetchGames(){
     fetch("http://localhost:3000/games")
     .then(response => response.json())
@@ -153,16 +142,97 @@ function fetchGames(){
 }
 
 function statsScores(games){
+    // console.log(games)
     sortedScores= games.sort(function(a, b){
     return parseFloat(a.totScore) - parseFloat(b.totScore);
     });
     let highScores= games.sort(function(a, b) {return a.totScore < b.totScore ? 1 : -1; })
     .slice(0, 10);
     console.log(highScores)
-    return highScores
-    // maxScores= sortedScores[0..5]
+    highScores
     //too tired to find the corresponding players right now
 }
+
+
+function pageButtons(){
+    let gameMenu= document.getElementById("menu")
+    let newButton= document.getElementById("newGame")
+    newButton.innerText= "New Game!"
+    newButton.addEventListener("click", e => {
+        newGame(e)
+    })
+
+    ///STATS MODAL
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    //What is in the modal
+    var content = document.getElementById("modal-header")
+    // content.innerText= statsScores()
+    //figure out how to show this in text
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
+} 
+
+
+////USER MODAL
+    // Get the modal
+//     var modal = document.getElementById("myModal");
+
+//     // Get the button that opens the modal
+//     var btn = document.getElementById("myBtn");
+
+//     // Get the <span> element that closes the modal
+//     var span = document.getElementsByClassName("close")[0];
+
+//     // When the user clicks the button, open the modal 
+//     btn.onclick = function() {
+//     modal.style.display = "block";
+//     }
+
+//     //What is in the modal
+//     var content = document.getElementById("modal-header")
+//     // content.innerText= statsScores()
+//     //figure out how to show this in text
+
+//     // When the user clicks on <span> (x), close the modal
+//     span.onclick = function() {
+//     modal.style.display = "none";
+//     }
+
+//     // When the user clicks anywhere outside of the modal, close it
+//     window.onclick = function(event) {
+//         if (event.target == modal) {
+//           modal.style.display = "none";
+//         }
+//     }
+// } 
+
+
+
+
+
 
 
 ////////////backup loop code
@@ -187,54 +257,3 @@ function statsScores(games){
 //     }
 //     break
 // }
-
-
-function pageButtons(){
-    let gameMenu= document.getElementById("menu")
-    let newButton= document.getElementById("newGame")
-    newButton.innerText= "New Game!"
-    newButton.addEventListener("click", e => {
-        newGame(e)
-    })
-    
-
-    
-
-
-    //"sets:" inner text
-    //display counter data
-    //timer as a stretch goal
-
-
-
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-    modal.style.display = "block";
-    }
-
-    //What is in the modal
-    var content = document.getElementById("modal-header")
-    content.innerText= statsScores()
-    //figure out how to show this in text
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-    }
-} 
